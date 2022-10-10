f12f3ebdf0d069e8d18237aea664a1fe2b96aaeb
Dekaizer89 <22204911@sun.ac.za>
2022-10-10 14:28:05 +0200

    foreign DNA function

---
+++ foreign_DNA.py
@@ -0,0 +1,29 @@
+def foreign_dna():
+    Genome_A = []
+    Genome_B = []
+    Genome_A1 = []
+    Genome_B1 = []
+    # check for genes present in Genome_A(source_genome) but absent from Genome_B(target_genome) and insert them into
+    # Genome_B1
+    for j in range(len(source_genome)):
+        Genome_A = source_genome[j]
+        Genome_B = target_genome[j]
+        for i in range(len(Genome_A)):
+            if Genome_A(i) not in Genome_B:
+                Genome_B.append(i, Genome_A(i))
+            Genome_B1.append(Genome_B)
+        return Genome_B1
+    # Let Genome_B1 undergo mutations via normal DCJ operations( call "Do mutations" function) to sort
+    # the source_genome into the target_genome. in the process it will produce Genome_A1, which will be converted
+    # to Genome_back into the source_genome
+    Gen_b_obj = Gen_Node.Node()
+    Genome_A1 = Gen_b_obj.Do mutations(Genome_A, Genome_B1)
+    return Genome_A1
+
+    # Check Genome_A1 for genes present in Genome_B but absent in Genome_A. Remove such genes from Genome_A1 to produce Genome_A which has been transformed into Genome_B with foreign_DNA inserted.
+    Genome_A2 = []
+    for j in range(len(Genome_A1)):
+            if genome_A1[j] in Genome_B and not in Genome_A:
+                Genome_A1.remove(j)
+            Genome_A2.append(Genome_A1)
+            return Genome_A2
